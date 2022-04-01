069cb9bb34424826940402e96beb1f3d346b8158
Foresteam <heler1141@gmail.com>
2022-04-01 17:14:03 +0300

    Moved to typescript

--- index.ts
+++ index.ts
@@ -1,37 +1,65 @@
-const path = require('path');
-const fs = require('fs');
-const { launch: chrome } = require('chrome-launcher');
+import { launch as chrome, LaunchedChrome } from 'chrome-launcher';
 
-const express = require('express');
-const { promisify } = require('util');
-const app = express();
-require('express-ws')(app);
+import express from 'express';
+import { promisify } from 'util';
+import expressWs, { WebsocketRequestHandler } from 'express-ws';
+import WS from 'ws';
 
-const node_functions = {};
+const app = expressWs(express()).app;
 
+const node_functions: {
+	[index: string]: CallableFunction,
+	/** Logs error to console */
+	error?: (...text: any[]) => void;
+} = {};
+
+interface Message {
+	reply?: any;
+	fid?: string;
+	func?: string;
+	args?: any[];
+	endInit?: boolean;
+	expose?: boolean;
+}
+interface StartParams {
+	/** Start page, e.g. index.html */
+	startPage?: string,
+	/** Directory where frontend resides */
+	webdir?: string,
+	closeOnExit?: boolean,
+	serverPort?: number,
+	/** Port where your frontend server is on. Useful when your HTTP and Websocket server is not the same thing. See my F0Talk repo for example of usage */
+	clientPort?: number,
+	/** Chromium should be found automatically, but if not, you can pass the path here */
+	chromiumPath?: string
+}
 class FWGUI {
+	[index: string]: any;
     static #Waiting = class Waiting {};
-    #waitingForReply = {};
 
-    #serve(dir, serverPort) {
-        return new Promise(resolve => {
+    #waitingForReply: { [index: string]: any } = {};
+	ws: WS;
+	endInit: boolean;
+
+    #serve(dir: string, serverPort: number) {
+        return new Promise<void>(resolve => {
             app.use(express.static(dir));
             app.use(express.static('node_modules/fwgui/frontend'));
             app.ws('/', async ws => {
                 this.ws = ws;
-                ws.on('message', msg => {
+                ws.on('message', (_msg: any) => {
                     try {
-                        msg = JSON.parse(msg);
+                        const msg: Message = JSON.parse(_msg);
                         // console.log(msg);
                         if (msg.endInit) {
                             this.endInit = true;
                             resolve();
                         }
                         else if (msg.expose) {
-                            this[msg.func] = async (...args) => {
+                            this[msg.func] = async (...args: any[]) => {
                                 let fid = `${Date.now().toString(16)}${Math.random().toString(16)}`;
                                 console.log(fid);
-                                ws.send(JSON.stringify({ func: msg.func, args, fid }));
+								ws.send(JSON.stringify(<Message>{ func: msg.func, args, fid }));
                                 this.#waitingForReply[fid] = new FWGUI.#Waiting();
                                 while (this.#waitingForReply[fid] instanceof FWGUI.#Waiting)
                                     await promisify(setTimeout)(5);
@@ -43,7 +71,7 @@ class FWGUI {
                         else if ('reply' in msg)
                             this.#waitingForReply[msg.fid] = msg.reply;
                         else
-                            (async () => ws.send(JSON.stringify({
+							(async () => ws.send(JSON.stringify(<Message>{
                                 reply: await (node_functions[msg.func])(...msg.args) || null,
                                 fid: msg.fid
                             })))();
@@ -56,23 +84,13 @@ class FWGUI {
             });
             let server = app.listen(serverPort);
             process.addListener('exit', () => server.close());
-            this.expose(function error(...text) { console.log('\x1b[31m', ...text); });
+            this.expose(function error(...text: any[]) { console.log('\x1b[31m', ...text); });
         })
     }
 
     constructor() {}
 
-    /**
-     * Start Express server, that will be backend
-     * @param {string} startPage Start page, e.g. index.html
-     * @param {string} webdir Directory where frontend resides
-     * @param {boolean} closeOnExit
-     * @param {number} serverPort
-     * @param {number} clientPort Useful when your HTTP and Websocket server is not the same thing. See my F0Talk repo for example of usage
-     * @param {string} chromePath Chromium should be found automatically, but if not, you can pass the path here
-     * @returns Chromium instance or false
-     */
-    async start({ startPage = '', webdir = 'wgui', closeOnExit = true, serverPort = 8080, clientPort, chromePath }) {
+    async start({ startPage = '', webdir = 'wgui', closeOnExit = true, serverPort = 8080, clientPort, chromiumPath }: StartParams): Promise<boolean | LaunchedChrome> {
         if (!webdir)
             return false;
         if (!clientPort)
@@ -81,10 +99,10 @@ class FWGUI {
 
         // setting up websocket server and running chrome
         const url = `http://localhost:${clientPort}/` + startPage;
-        let success = false;
+        let success: LaunchedChrome | boolean = false;
         await chrome({
             chromeFlags: [`--app=${url}`, '--window-size=1280,720'],
-            chromePath: chromePath || undefined,
+            chromePath: chromiumPath || undefined,
             ignoreDefaultFlags: true
         }).then(instance => {
             if (closeOnExit) {
@@ -98,47 +116,44 @@ class FWGUI {
     }
     /**
      * Expose backend function to frontend
-     * @param {function|string} funcname JS function or frontend alias
-     * @param {function} func JS function, if alias was passed before
+     * @param funcname JS function or frontend alias
+     * @param func JS function, if alias was passed before
      */
-    async expose(funcname, func = null) {
+    async expose(funcname: CallableFunction | string, func: CallableFunction | null = null) {
         if (!funcname)
             return;
         if (!func) {
-            func = funcname;
-            funcname = funcname.name;
+            func = funcname as CallableFunction;
+            funcname = (funcname as CallableFunction).name;
         }
-        node_functions[funcname] = func;
+        node_functions[funcname as string] = func;
         while (!this.ws)
             await promisify(setTimeout)(50);
-        this.ws.send(JSON.stringify({
+        this.ws.send(JSON.stringify(<Message>{
             func: funcname,
             expose: true
         }));
     }
-    /**
-     * Tell the frontend that all functions are defined
-     */
+    /** Tell the frontend that all functions are defined */
     async endExpose() {
         while (!this.ws || !this.endInit)
             await promisify(setTimeout)(50);
-        this.ws.send(JSON.stringify({
+        this.ws.send(JSON.stringify(<Message>{
             endExpose: true
         }));
     }
     /**
      * AKA publish (the frontend is the subscriber)
-     * @param {string} event Function name
-     * @param  {...any} args
+     * @param event Name
+     * @param args The arguments
      */
-    async emit(event, ...args) {
+    async emit(event: string, ...args: any[]) {
         if (!this.ws)
             return;
-        this.ws.send(JSON.stringify({
+		this.ws.send(JSON.stringify(<Message>{
             event,
             args
         }));
     }
-};
-
-module.exports = () => new FWGUI();
+}
+export default () => new FWGUI();
